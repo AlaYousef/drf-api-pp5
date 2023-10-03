0333faddc6cca37aa974f49ee68cc97dd0101a32
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-10-03 10:25:54 +0000

    create permissions.py to add custom permissions

--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
-
+from drf_api_pp5.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
@@ -13,7 +13,9 @@ class ProfileList(APIView):
     """
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many= True)
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request': request}
+        )
         return Response(serializer.data)
 
 class ProfileDetail(APIView):
@@ -23,19 +25,27 @@ class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
 
     """
+    Connect permission with ProfileDetail view
+    """
+    permission_classes =[IsOwnerOrReadOnly]
+    """
     Get methods to return a profile by id and 
     return 404 error if it doesn't exist
     """
     def get_object(self, pk):
         try:
             profile=Profile.objects.get(pk=pk)
+            self.check_object_permissions (self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer=ProfileSerializer(profile)
+        serializer = ProfileSerializer(
+            profile, context={'request': request}
+        )
+
         return Response(serializer.data)
 
     def put (self, request, pk):
@@ -43,7 +53,9 @@ class ProfileDetail(APIView):
         update a specific profile and save it if it is valid
         """
         profile =self.get_object(pk)
-        serializer=ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
