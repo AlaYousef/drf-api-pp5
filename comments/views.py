cea382273d93da59c23dc4bccbd27df40010ef50
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-10-04 10:54:06 +0000

    create commentlist/ comment detail views using generic api view

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api_pp5.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+class CommentList(generics.ListCreateAPIView):
+    """
+    List / create a comment for logged in users.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment/ update or delete it by id for comments owner.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
