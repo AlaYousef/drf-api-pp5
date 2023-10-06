2d5f5ab076f5357d2cd0f1489495f9962c717b08
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-10-06 09:28:17 +0000

    add date and time formatting for created and updated fields

--- drf_api_pp5/settings.py
+++ drf_api_pp5/settings.py
@@ -77,9 +77,14 @@ REST_FRAMEWORK = {
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 7,
-    
+    'PAGE_SIZE': 7,   
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
